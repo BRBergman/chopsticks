d45a020f1952e2bf84cbdef2adf88de298393725
BRBergman <blake.bergman@gmail.com>
2022-12-16 14:15:33 -0500

    no more submodule

---
+++ src/printnc.cpp
@@ -0,0 +1,63 @@
+#include "printnc.h"
+#include <stdarg.h>
+
+
+int printnccenter (const char *format, ...)
+{
+   	char buffer[256];
+  	va_list args;
+  	va_start (args, format);
+  	int done = vsprintf (buffer,format, args);
+  	va_end (args);
+	std::string in = buffer;
+	mvprintw(row/2,(col-done)/2,"%s",in.c_str()); //print string to center of screen
+	return done;
+}
+//		mvprintw(row/2-7+i, (col-strlen(askev[i])-11)/2, "Press %i to %s",i,askev[i] );
+
+int printnccenter (int ver, int hoz, const char *format, ...)
+{
+   	char buffer[256];
+  	va_list args;
+  	va_start (args, format);
+  	int done = vsprintf (buffer,format, args);
+  	va_end (args);
+	std::string in = buffer;
+	mvprintw(row/2-ver,(col-done)/2-hoz,"%s",in.c_str()); //print string to center of screen
+	return done;
+}
+
+int printnc (int ver, int hoz, const char *format, ...)
+{
+   	char buffer[256];
+  	va_list args;
+  	va_start (args, format);
+  	int done = vsprintf (buffer,format, args);
+  	va_end (args);
+	std::string in = buffer;
+	mvprintw(ver,hoz,"%s",in.c_str()); //print string to center of screen
+	return done;
+}
+int printnc (int ver, const char *format, ...)
+{
+   	char buffer[256];
+  	va_list args;
+  	va_start (args, format);
+  	int done = vsprintf (buffer,format, args);
+  	va_end (args);
+	std::string in = buffer;
+	mvprintw(ver,0,"%s",in.c_str()); //print string to center of screen
+	return done;
+}
+
+int printnc (const char *format, ...)
+{
+   	char buffer[256];
+  	va_list args;
+  	va_start (args, format);
+  	int done = vsprintf (buffer,format, args);
+  	va_end (args);
+	std::string in = buffer;
+	mvprintw(0,0,"%s",in.c_str()); //print string to center of screen
+	return done;
+}
---
+++ src/printnc.h
@@ -0,0 +1,22 @@
+#ifndef PRINTNC_H
+#define PRINTNC_H
+#include <ncurses.h>
+#include <iostream>
+#include <stdarg.h>
+int row, col;
+
+//print a message to the center of the screen, returns length 
+int printnccenter (const char *format, ...);
+//print a message to the center of the screen, add offset from middle, returns length
+int printnccenter (int ver, int hoz, const char *format, ...);
+//prints message to the left of the screen, ver to go down, hoz to go to the side
+int printnc (int ver, int hoz, const char *format, ...);
+//prints message to the left of the screen, ver to go down
+int printnc (int ver, const char *format, ...);
+//prints message to top left of the screen
+int printnc (const char *format, ...);
+
+
+
+
+#endif
